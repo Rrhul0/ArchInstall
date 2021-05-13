76ad2d596fe3e27f851313d66be9bdffbe7fedec
Rrhul <72130755+Rrhul0@users.noreply.github.com>
2021-05-13 13:35:04 +0530

    Update create-root.sh

--- create-root.sh
+++ create-root.sh
@@ -4,7 +4,8 @@ echo "this script will not change disk partition size, add or delete so we sugge
 sleep 5
 
 echo "testing your internet connection"
-if [[ $(ping -w 2 google.com | grep -c transmitted) -eq 1 ]]
+ping -c 1 google.com
+if [[ $? -eq 1 ]]
 then
     echo 'connected to internet'
 else
@@ -15,13 +16,19 @@ fi
 echo "make sure your partitions ready"
 echo "mounting partitions in /mnt"
 lsblk
-echo "enter EFI partition name like sda1 or sda2 (commonly this partition is in size of 200MB to 1GB and found in starting of disk)"
+echo "enter EFI partition name like sda1 or sda2 or lvme(commonly this partition is in size of 200MB to 1GB and found in starting of disk)"
 read efi
-echo "enter root partition name just like before like sda1 or sda2"
+echo "enter root partition name just like before like sda1 or sda2 or lvme"
 read root
 echo "enter filesystem type for root partition eg. ext4, btrfs (default ext4) WARNING:All data of this partition will be destroyed"
 read fs-root
 echo "enter home partition name (if not have dedicated home partiton just leave it empty)"
 read home
-if [[ $home == ]]
+if [[ $home == sd* ]] | [[ $home == lvme* ]]
+then
+    echo "enter filesystem type for home partition eg. ext4, btrfs (default ext4) WARNING:All data of this partition will be destroyed"
+ else
+ 
+ fi
+
 echo "we will use"
