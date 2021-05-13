b1560fad48b8794194f03cdb47b3bfa0f18eacc1
Rrhul <72130755+Rrhul0@users.noreply.github.com>
2021-05-13 12:20:54 +0530

    Update create-root.sh

--- create-root.sh
+++ create-root.sh
@@ -17,4 +17,11 @@ echo "mounting partitions in /mnt"
 lsblk
 echo "enter EFI partition name like sda1 or sda2 (commonly this partition is in size of 200MB to 1GB and found in starting of disk)"
 read efi
-echo "enter root partition name just like before like sda1 or sda2 ()"
+echo "enter root partition name just like before like sda1 or sda2"
+read root
+echo "enter filesystem type for root partition eg. ext4, btrfs (default ext4) WARNING:All data of this partition will be destroyed"
+read fs-root
+echo "enter home partition name (if not have dedicated home partiton just leave it empty)"
+read home
+if [[ $home == ]]
+echo "we will use"
